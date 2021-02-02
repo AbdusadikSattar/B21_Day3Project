67e7252eb6c745cccb92a6a79f8a58a3ddda9633
AbdusadikSattar <71940275+AbdusadikSattar@users.noreply.github.com>
2021-02-02 20:16:48 +0100

    added some more code in new class

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -1,5 +1,6 @@
 public class Cocomelon {
     public static void main(String[] args) {
         System.out.println("Created new branch and new class");
+        System.out.println("Added some new message in new class");
     }
 }
